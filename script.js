ade39ebcc8e0bef0f8f73c3713dc7d37f95ccb44
Laurynas Gailius <150287011+LaurynasGHub@users.noreply.github.com>
2024-01-04 21:05:00 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// let points = 150;
+
+// let type = points > 100 ? 'gold' : 'silver';
+
+// console.log(type);
+
+//sukurti elementa kur butu gridas kryziukam nuliukam
+//langeliam priskiriam id nuo 1 iki 9
+//jei zmogus paspaudzia kazkur tai pakeiciam ir state ir paveiksliuka i X
+//sudedam paspaustas value i array kur bus visi X
+//tada parenkam random skaiciu is array? isbrauksim ten pvz 1, 3,5 ir tada is 2,4,6,7,8,9 rinks kur det O
+//ten ideda O ir ideda i array ta reiksme?
