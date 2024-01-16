dd25160479d0cf132308756b35e7e6c7409af3a0
Laurynas Gailius <150287011+LaurynasGHub@users.noreply.github.com>
2024-01-16 22:54:28 +0200

    fixed issue with selected class, class wasnt added to a button when X was added

--- script.js
+++ script.js
@@ -3,14 +3,13 @@ function addCriss(btnId) {
   mygtukas.style.border = '10px solid black';
   mygtukas.style.borderRadius = '100%';
   console.log('btnId= ' + btnId);
-  mygtukas.classList.add('selected0'); //add class to selected btn
-  //need to add class selected0 to xSvg that has the same id
-  //so it would fall out of selection list
+  mygtukas.classList.add('selected'); //add class to selected btn
+
   let idNr = btnId.charAt(btnId.length - 1);
   console.log('idNr-', idNr);
 
   let svgMygtukas = document.getElementById('xSvg' + idNr);
-  svgMygtukas.classList.add('selected0');
+  svgMygtukas.classList.add('selected');
 
   getUnselectedBtn();
 }
@@ -38,43 +37,65 @@ function getUnselectedBtn() {
       gaunamasMygtukas = document.getElementById(rndBtnId);
       stringToCheck = String(gaunamasMygtukas.classList);
 
-      if (stringToCheck.indexOf('selected0') > -1) {
-        console.log('turi selected0', rndBtnId);
-        console.log(
-          'indexOf class selected0',
-          stringToCheck.indexOf('selected0') > -1
-        );
+      console.log('stringToCheck-', stringToCheck);
+
+      if (stringToCheck.indexOf('selected') > -1) {
+        console.log('turi selected', rndBtnId);
         rndNumber = getRandomInt(1, 10);
         rndBtnId = 'mygtukas' + rndNumber;
       } else {
-        console.log('neturi selected0', rndBtnId); //if element has style then this returns false even though the element has the class
-        console.log(
-          'indexOf class selected0',
-          stringToCheck.indexOf('selected0') > -1
-        );
+        //if the same id svg is selected btn doesnt get the selected class and shows as false when checked for availability
+        console.log('neturi selected', rndBtnId);
         mygtukasRastas = true;
         addX(rndNumber);
       }
     }
     if (itirNr >= 20) {
       console.log('itirNr more than 20');
+      mygtukasRastas = true;
     }
   }
 }
 
+//function adds X to random selected spot
 function addX(btndID) {
   setTimeout(() => {
     console.log('xSvg' + btndID);
     let selBtn = document.getElementById('xSvg' + btndID);
-    selBtn.classList.add('selected0'); //add class to selected btn
+    selBtn.classList.add('selected'); //add class to selected svg
     selBtn.style.display = 'block';
+    //add class selected to a button
+    selBtn = document.getElementById('mygtukas' + btndID);
+    selBtn.classList.add('selected');
   }, 1000);
 }
 
+//function gets random number between 2 values
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+
+//function to check if selected item has selected class
+function checkSelected(elementId, elementType) {
+  elementToCheck = elementType + elementId;
+  console.log('===checkSelected===');
+  console.log('elementToCheck-', elementToCheck);
+
+  let checkingElement = document.getElementById(elementToCheck);
+
+  let stringToCheck = String(checkingElement.classList);
+
+  console.log('stringToCheck-', stringToCheck);
+
+  if (stringToCheck.indexOf('selected') > -1) {
+    console.log(elementToCheck, 'has a selected class');
+    return (checkSelected = true);
+  } else {
+    console.log(elementToCheck, 'doesnt have a selected class');
+    return (checkSelected = false);
+  }
 }
 
 //function for winning conditions
@@ -83,3 +104,7 @@ function winningTheGame() {
   //make array with selected0 class as a picture
   //compare that array
 }
+
+//TO DO LIST
+//need to fix svg, now it just floats randomly
+//winning conditions need to be added
